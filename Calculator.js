36de965333a6b1461045a2199f5e202c5b1bf604
Sergey Sherstobitov <in43sh@gmail.com>
2021-08-23 01:28:15 -0700

    Update metadate in package.json file.

--- Calculator.js
+++ Calculator.js
@@ -1,20 +1,14 @@
-class Calculator{
-
-  constructor()
-  {
+class Calculator {
+  constructor() {
     console.log("Calculator loaded");
   }
 
-add(num1=0,num2=0){
-  
-   if(typeof(num1)!== 'number' ||
-   typeof(num2)!== 'number'){
-     return 0;
-   }
-   return num1 + num2;
- }
-
-
+  add(num1 = 0, num2 = 0) {
+    if (typeof num1 !== "number" || typeof num2 !== "number") {
+      return 0;
+    }
+    return num1 + num2;
+  }
 }
 
 //required so data is visible externally
